bc46d4bc54a31f34a1752f36d0be12865af5d869
Derek <sjukledighet@gmail.com>
2020-11-17 23:37:22 -0500

    Add complete solution for day 3

--- p03.py
+++ p03.py
@@ -1,10 +1,24 @@
 from aocd import data
 
+
 possible = 0
+rows = []
 
 for triangle in data.split('\n'):
-    sides = sorted(int(side) for side in triangle.split())
+    sides = [int(side) for side in triangle.split()]
+    rows.append(sides)
+    sides = sorted(sides)
     if sum(sides[:2]) > sides[-1]:
         possible += 1
 
-print(possible)
+print('Part 1:', possible)
+
+possible = 0
+vert_triangles = [side for column in zip(*rows) for side in column]
+
+for triangle in zip(*[iter(vert_triangles)]*3):
+    sides = sorted(triangle)
+    if sum(sides[:2]) > sides[-1]:
+        possible += 1
+
+print('Part 2:', possible)
